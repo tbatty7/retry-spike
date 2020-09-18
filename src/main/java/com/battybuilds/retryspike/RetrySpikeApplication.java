9b76c9f90f186e9a5f2bdcc64eb475d34c3161d4
Tim B <tbatty7@gmail.com>
2020-09-17 21:14:45 -0400

    initial commit

---
+++ src/main/java/com/battybuilds/retryspike/RetrySpikeApplication.java
@@ -0,0 +1,13 @@
+package com.battybuilds.retryspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrySpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrySpikeApplication.class, args);
+	}
+
+}
